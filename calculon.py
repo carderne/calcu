12880aeb4ba218938d7795abc611ea192fd505b3
Chris Arderne <chris@rdrn.me>
2020-01-14 10:15:58 +0100

    Disable custom decimals; allow spaces

--- calculon.py
+++ calculon.py
@@ -7,23 +7,18 @@ import sys
 from math import *
 
 
-def main(query=None, decimals=None):
-    query = query.replace("[", "(").replace("]", ")")
+def main(query=None):
+    query = query.replace("[", "(").replace("]", ")").replace("^", "**")
     try:
         res = eval(query)
-        if decimals is None:
-            if isinstance(res, int) or abs(int(res) - res) < 1e-9:
-                decimals = 0
-            else:
-                decimals = 3
+        if isinstance(res, int) or abs(int(res) - res) < 1e-9:
+            decimals = 0
+        else:
+            decimals = 3
         print("\t\t= {:.{}f}".format(res, decimals))
     except SyntaxError:
         print("\t\tError")
 
 
 if __name__ == "__main__":
-    args = sys.argv
-    main(
-        query=args[1] if len(args) >= 2 else None,
-        decimals=args[2] if len(args) >= 3 else None,
-    )
+    main("".join(sys.argv[1:]))
