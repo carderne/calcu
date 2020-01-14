4ac5ba9aa42f5f47291ca9d5f79b07ec0827ab26
Chris Arderne <chris@rdrn.me>
2020-01-14 10:23:42 +0100

    Calculate required decimals, limit to 9

--- calculon.py
+++ calculon.py
@@ -7,15 +7,20 @@ import sys
 from math import *
 
 
+def estimate_decimals(res):
+    if isinstance(res, int) or abs(int(res) - res) < 1e-9:
+        return 0
+    for i in range(9):
+        if (res * 10**i) % 1 == 0.0:
+            return i
+    return 9
+
+
 def main(query=None):
     query = query.replace("[", "(").replace("]", ")").replace("^", "**")
     try:
         res = eval(query)
-        if isinstance(res, int) or abs(int(res) - res) < 1e-9:
-            decimals = 0
-        else:
-            decimals = 3
-        print("\t\t= {:.{}f}".format(res, decimals))
+        print("\t\t= {:.{}f}".format(res, estimate_decimals(res)))
     except SyntaxError:
         print("\t\tError")
 
