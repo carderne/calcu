ff97057da9a8b087a017c8033a57d414941703d7
Chris Arderne <chris@rdrn.me>
2020-01-15 11:02:04 +0100

    Add multiplication symbol handling

--- calculon.py
+++ calculon.py
@@ -11,13 +11,15 @@ def estimate_decimals(res):
     if isinstance(res, int) or abs(int(res) - res) < 1e-9:
         return 0
     for i in range(9):
-        if (res * 10**i) % 1 == 0.0:
+        if (res * 10 ** i) % 1 == 0.0:
             return i
     return 9
 
 
 def main(query=None):
-    query = query.replace("[", "(").replace("]", ")").replace("^", "**")
+    query = (
+        query.replace("[", "(").replace("]", ")").replace("^", "**").replace("x", "*")
+    )
     try:
         res = eval(query)
         print("\t\t= {:.{}f}".format(res, estimate_decimals(res)))
