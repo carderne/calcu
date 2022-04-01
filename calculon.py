b3bb9e828720ed8b87b6fd965fc535f7b40cb05e
Chris Arderne <chris@rdrn.me>
2022-04-01 08:45:03 +0100

    Use 1e10 precision for decimals, rather than 0

--- calculon.py
+++ calculon.py
@@ -3,17 +3,18 @@
 """Perform simple command-line based calculations."""
 
 import sys
-from math import *
+from math import *  # noqa
 from pathlib import Path
 
 last_path = Path("~/.config/calculon.last").expanduser()
+precision = 1e-10
 
 
 def estimate_decimals(res):
     if isinstance(res, int) or abs(int(res) - res) < 1e-9:
         return 0
     for i in range(9):
-        if (res * 10**i) % 1 == 0.0:
+        if (res * 10**i) % 1 < precision:
             return i
     return 9
 
