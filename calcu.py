d307df2863a429a5ecb23c12a24e81cd4532d67a
Chris Arderne <chris@rdrn.me>
2022-08-15 11:18:11 -0400

    Change to pyproject.toml setup

--- calcu.py
+++ calcu.py
@@ -5,36 +5,41 @@
 import sys
 from math import *  # noqa
 from pathlib import Path
+from typing import Any
 
-__version__ = "0.9.8"
+__version__ = "0.9.9"
 
 last_path = Path("~/.config/calcu.last").expanduser()
-precision = 1e-10
+PRECISION = 1e-10
 
 
-def estimate_decimals(res):
+def estimate_decimals(res: float) -> int:
+    """Estimate number of decimals to show."""
     if isinstance(res, int) or abs(int(res) - res) < 1e-9:
         return 0
     for i in range(9):
-        if (res * 10**i) % 1 < precision:
+        if (res * 10**i) % 1 < PRECISION:
             return i
     return 9
 
 
-def load_last():
+def load_last() -> str:
+    """Load saved value from previous run."""
     try:
-        with last_path.open() as f:
-            return str(float(f.read().strip()))
+        with last_path.open("r") as file:
+            return str(float(file.read().strip()))
     except FileNotFoundError:
         return ""
 
 
-def dump_last(res):
-    with last_path.open("w") as f:
-        print(res, file=f)
+def dump_last(res: Any) -> None:
+    """Save value to file."""
+    with last_path.open("w") as file:
+        print(res, file=file)
 
 
-def main(query=""):
+def main(query: str = "") -> None:
+    """Evaluate query and print result."""
     last = load_last()
     query = (
         query.replace("[", "(")
@@ -44,14 +49,15 @@ def main(query=""):
         .replace("_", last)
     )
     try:
-        res = eval(query)
+        res = eval(query)  # pylint: disable=eval-used
         dump_last(res)
         print(f"\t\t= {res:,.{estimate_decimals(res)}f}")
-    except (SyntaxError, NameError) as e:
-        print("\t\tError", e)
+    except (SyntaxError, NameError) as ex:
+        print("\t\tError", ex)
 
 
-def cli():
+def cli() -> None:
+    """CLI entrypoint."""
     if len(sys.argv) > 1:
         if sys.argv[1] == "--version":
             print(__version__)
